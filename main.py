b83bd3ebd55dcf259cf9bd7c707379deba739da9
Austin <ab.granbery@gmail.com>
2026-07-07 21:01:29 -0500

    Continued work on cleaning up code and focusing on fail-safes if api is  down.

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import requests
 
 def get_geo_data(city):
     geocoding_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=5&language=en&format=json"
-    response = requests.get(geocoding_url, timeout=10)
-    geo_data = response.json()
-    result = geo_data.get('results')
-    return result
+
+    try:
+        response = requests.get(geocoding_url, timeout=10)
+        response.raise_for_status()
+        geo_data = response.json()
+        result = geo_data.get('results')
+        return result
+    except requests.exceptions.RequestException:
+        return None
 
 def get_weather_data(latitude, longitude):
     forecast_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,relative_humidity_2m,precipitation,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch"
-    forecast_response = requests.get(forecast_url, timeout=10)
-    forecast_data = forecast_response.json()
-    return forecast_data
+    try:
+        forecast_response = requests.get(forecast_url, timeout=10)
+        forecast_response.raise_for_status()
+        forecast_data = forecast_response.json()
+        return forecast_data
+    except requests.exceptions.RequestException:
+        print("Something went wrong. Please try again later")
+        return None
 
 def display_possible_matches(displayed_matches):
     for number, matches in enumerate(displayed_matches, start=1):
@@ -51,15 +61,21 @@ def ask_to_continue():
 
 def main():
     getting_weather = True
+    print("Welcome to my weather app.")
 
     while getting_weather:
 
         weather_results = True
         city_name = input("What city?: ")
         possible_matches = get_geo_data(city_name)
+        if possible_matches is None:
+            print("Something went wrong. Please try again later.")
+            break
+
         if not possible_matches:
-            print("Invalid entry. Please enter a  valid city.")
+            print("Invalid entry. Please enter a valid city.")
             continue
+
         displayed_matches = possible_matches[:5]
 
         while weather_results:
@@ -74,8 +90,11 @@ def main():
                     latitude = selected_city.get('latitude')
 
                     forecast_data = get_weather_data(latitude, longitude)
-
-                    display_weather_report(selected_city, forecast_data)
+                    if forecast_data is None:
+                        print("Weather could not be loaded. Please try again later.")
+                        break
+                    else:
+                        display_weather_report(selected_city, forecast_data)
 
                     search_again = ask_to_continue()
                     if search_again:
